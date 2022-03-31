ca859a6d1d35aa8ffeefa6404083bc383b5a5bc2
unknown <seafoodgarden@gmail.com>
2022-03-31 08:50:49 -0700

    changes

--- A2/notes/fizzbuzz.sh
+++ A2/notes/fizzbuzz.sh
@@ -1,21 +1,32 @@
 i=1 
 until [i <= 20]
-do (($i % 3)) && (($i % 5))
+do (($i % 3 && $i % 5))
 i=$(( i+1 ))
 echo $i
-if (($i % 3 == 0))
+then
+ if (($i % 3 == 0))
+ then
 echo 'fizz'
 break
-if (($i % 5 == 0))
+  then
+ if (($i % 5 == 0))
 echo 'buzz'
 break
-if (($i % 3 == 0)) && (($i % 5 == 0))
+  then
+ if (($i % 3 == 0)) && (($i % 5 == 0))
 echo 'FizzBuzz'
 break
-else
+  then
+ else
+ fi
+fi 
 echo $i
-fi
+
 done
+
+
+
+
 <<comment
 then
 echo
